76e229c5726731a02071b813fac144b1f9abf624
Firdavs Toshev <firdavs080902@gmail.com>
2024-10-15 20:09:21 +0500

    feat: process events in goroutines

--- worker/worker.go
+++ worker/worker.go
@@ -2,13 +2,14 @@ package worker
 
 import (
 	"fmt"
-
 	"notification-service/domain"
 )
 
 func ProcessEvents(events chan domain.Event) {
 	for event := range events {
-		// Имитация обработки ивента
-		fmt.Printf("Processed: %+v", event)
+		go func(e domain.Event) {
+			// Имитация обработки ивента
+			fmt.Printf("Processed: %+v", event)
+		}(event)
 	}
 }
